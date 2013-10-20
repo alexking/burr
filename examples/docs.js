65a515e6f34221e5cf59eadbd8d8e6bc2c8c2b48
Alex King <alexking@me.com>
2013-10-19 23:49:07 -0400

    Updating example docs

--- examples/docs.js
+++ examples/docs.js
@@ -1,15 +1,34 @@
 
-function Apple() {
+function Fruit() {
 
 	this.chomps = 0;
 
 }
 
-Apple.prototype.chomp = function() {
+Fruit.prototype.chomp = function() {
 
 	this.chomps++;
 
 };
+
+
+
+function Apple() {
+
+	var t = this;
+	[Fruit].forEach(function(mixin) {
+		for (var func in mixin.prototype) {
+			Apple.prototype[func] = mixin.prototype[func];
+		}
+		var mixinInstance = new mixin();
+		for (var prop in mixinInstance) {
+			t[prop] = mixinInstance[prop];
+		}
+	});
+
+}
+
+
 
 var apple = new Apple();
 apple.chomp();
